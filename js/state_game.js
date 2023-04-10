eec3335da6dd1407b8d7bfda4abc11bdf649f4cb
J Sprague <jeffrsprague@gmail.com>
2023-04-09 19:46:03 -0700

    Removed MAP from main page - now press 'M' to toggle it on and off. Add crosshair for where you are on the map.

--- js/state_game.js
+++ js/state_game.js
@@ -13,6 +13,7 @@ var GAME = {};
     log(this.id + ' Loaded');
 
     var INITED = false;
+    var MAP_ACTIVE  = false;
 
     var tileMapCanvas = undefined;
     var mapCanvas = undefined;
@@ -39,7 +40,21 @@ var GAME = {};
 
     // ********************************************
     this.eventHandler = function (event) {
-        return false;
+        var consumed = false;
+        var key = event.key;
+        var keyCode = event.keyCode;
+                
+        console.log("Event: " + keyCode);
+
+        switch (keyCode) {
+            case jgl.KEYS.M: {
+                MAP_ACTIVE = !MAP_ACTIVE;
+                consumed = true;
+                this.redraw();
+            }
+        }
+
+        return consumed;
     }.bind(this);
 
     // ********************************************
@@ -49,11 +64,11 @@ var GAME = {};
 
         tileMapCanvas = document.createElement("canvas");
         tileMapCanvas.width = 642;
-        tileMapCanvas.height = 386;
+        tileMapCanvas.height = 386 + 256;
 
         mapCanvas = document.createElement("canvas");
-        mapCanvas.width = 524;
-        mapCanvas.height = 329;
+        mapCanvas.width = 500;
+        mapCanvas.height = 320;
 
         panelCanvas = document.createElement("canvas");
         panelCanvas.width = 644;
@@ -72,10 +87,10 @@ var GAME = {};
             // Draw background
             g.ctx.drawImage(g.woodImg, 0, 0, 1000, 610, 0, 0, s.PLAYFIELD_WIDTH - 0, s.PLAYFIELD_HEIGHT - 0);
 
-            drawMap();
             drawEnvironment();
             drawTileMap(); 
             drawPanel();
+            drawMap();
         }
 
     };
@@ -91,7 +106,7 @@ var GAME = {};
             console.log(i+": "+jgl.decToHex(pixels.data[i])+jgl.decToHex(pixels.data[i+1])+jgl.decToHex(pixels.data[i+2]) );
         }
 
-        for (y = 0; y < 3; y++) {
+        for (y = 0; y < 5; y++) {
             for (x = 0; x < 5; x++) {
                 if (Math.random() > .5)
                     tileMapCtx.drawImage(g.beachTile, 0, 0, 128, 128, 2 + (x * 128), 2 + (y * 128), 126, 126);
@@ -105,7 +120,7 @@ var GAME = {};
         tileMapCtx.lineWidth = 2;
         tileMapCtx.strokeRect(0, 0, 128*5 + 2, 128*3 + 2);
 
-        g.ctx.drawImage(tileMapCanvas, 38, 21, 128*5 + 2, 128*3 + 2);
+        g.ctx.drawImage(tileMapCanvas, 38, 26, 128*5 + 2, 128*5 + 2);
 };
 
     // ********************************************
@@ -115,21 +130,43 @@ var GAME = {};
         ctx.drawImage(g.beachImg, 0, 0, 1024, 725, 40, 35, 450, 330);
         ctx.drawImage(g.frameImg, 0, 0, 476, 352, 0, 0, 540, 424);
 
-        g.ctx.drawImage(environmentCanvas, 714, 0, 540, 424);
+        g.ctx.drawImage(environmentCanvas, 714, 10, 540, 424);
     };
 
     // ********************************************
     drawMap = function() {
         // Draw map
+        if (!MAP_ACTIVE) 
+            return;
 
         var ctx = mapCanvas.getContext("2d");
-        ctx.drawImage(g.mapImg, 0, 0, 500, 320, 0, 0, 520, 325);
+        ctx.drawImage(g.mapImg, 0, 0);
+
+        ctx.strokeStyle = "#C20";
+        ctx.lineWidth = 3;
+        ctx.beginPath();
+        ctx.moveTo(g.xTile, g.yTile - 10);
+        ctx.lineTo(g.xTile, g.yTile - 2);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(g.xTile - 10, g.yTile);
+        ctx.lineTo(g.xTile - 2, g.yTile);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(g.xTile, g.yTile + 2);
+        ctx.lineTo(g.xTile, g.yTile + 10);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(g.xTile + 2, g.yTile);
+        ctx.lineTo(g.xTile + 10, g.yTile);
+        ctx.stroke();
+
+        ctx.lineWidth = 2;
         ctx.strokeStyle = "#fc8";
         ctx.strokeStyle = 'rgba(255,225,176,0.5)';
-        ctx.lineWidth = 2;
-        ctx.strokeRect(0, 0, 520 + 4, 325 + 4);
+        ctx.strokeRect(0, 0, 500, 320);
 
-        g.ctx.drawImage(mapCanvas, 718, 438, 524, 329);
+        g.ctx.drawImage(mapCanvas, 0, 0, 500, 320, 140, 80, 1000, 640);
      };
 
     // ********************************************
@@ -141,7 +178,7 @@ var GAME = {};
         ctx.lineWidth = 2;
         ctx.strokeRect(0, 0, 644, 329);
 
-        g.ctx.drawImage(panelCanvas, 38, 438, 644, 329);
+        g.ctx.drawImage(panelCanvas, 718, 448, 524, 329);
     };
 
 }).apply(GAME);  // Apply this object to the State placeholder we defined
